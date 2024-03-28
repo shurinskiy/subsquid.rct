1180054fc3beef5031f7ace8671a00711a9a2950
_tango <shurinskiy@mail.ru>
2024-03-28 16:17:32 +0500

    add semantic tags

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import Footer from "./components/Footer/Footer"
 import Header from "./components/Header/Header"
 import Hero from "./components/Hero/Hero"
 import Cards from "./components/Cards/Cards"
 import Partner from "./components/Partner/Partner"
+import Footer from "./components/Footer/Footer"
 
 function App() {
 
